d41d6248850132994a1a8554931ff5a8bebea24f
kidist997 <kidistamanuel22@gmail.com>
2024-09-30 03:26:20 +1100

    Added the hidden bar.
    Added the hidden nav and styled it. And also added JavaScript for interaction.

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,22 @@ new Typewriter("#paragraph2", {
   delay: 150,
   cursor: " ",
 });
+
+const menuIcon = document.getElementById("menu-icon");
+const pageContent = document.querySelector(".nav-bar");
+const sideNav = document.getElementById("hidden-nav");
+
+menuIcon.addEventListener("click", () => {
+  pageContent.classList.toggle("menu-active");
+  sideNav.classList.toggle("menu-active");
+});
+
+const currentPath = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".hidden-nav a");
+navLinks.forEach((link) => {
+  const href = link.getAttribute("href");
+
+  if (href === currentPath) {
+    link.classList.add("active");
+  }
+});
