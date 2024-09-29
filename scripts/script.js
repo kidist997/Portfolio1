5f7cc7797f511a85938d6d211729bebe0f616989
kidist997 <kidistamanuel22@gmail.com>
2024-09-30 08:07:49 +1100

    Added a 3D image and put animation on it using CSS.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,3 @@
-new Typewriter("#paragraph2", {
-  strings: ["Front - End  Developer"],
-
-  autoStart: true,
-  loop: true,
-  delay: 150,
-  cursor: " ",
-});
-
 const menuIcon = document.getElementById("menu-icon");
 const pageContent = document.querySelector(".nav-bar");
 const sideNav = document.getElementById("hidden-nav");
@@ -24,4 +15,13 @@ navLinks.forEach((link) => {
   if (href === currentPath) {
     link.classList.add("active");
   }
+});
+
+new Typewriter("#paragraph2", {
+  strings: ["Front - End  Developer"],
+
+  autoStart: true,
+  loop: true,
+  delay: 150,
+  cursor: " ",
 });
